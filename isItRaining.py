c508eac1bae1880c30f7b13a2f0909a20002a19e
cara <chuynh5@uw.edu>
2024-11-12 10:40:07 -0800

    finished inclass

--- isItRaining.py
+++ isItRaining.py
@@ -1,9 +1,11 @@
 import urllib.request
 import json
 with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
-    is_it_raining_in_seattle = response.read().decode()
+    is_it_raining_in_seattle = response.read().decode().strip().lower()
 
 if is_it_raining_in_seattle == "true":
     print("Yes!")
-else:
+elif is_it_raining_in_seattle == "false":
     print("No!")
+else:
+    print("Unexpected response:", is_it_raining_in_seattle)
