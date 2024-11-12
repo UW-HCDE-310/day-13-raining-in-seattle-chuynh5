809fcfa0df3699d71e610f45425c1d05c37fa9ed
cara <chuynh5@uw.edu>
2024-11-12 10:53:33 -0800

    in class assignment finished

--- isItRaining.py
+++ isItRaining.py
@@ -1,11 +1,16 @@
-import urllib.request
-import json
-with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
-    is_it_raining_in_seattle = response.read().decode().strip().lower()
+import urllib.parse, urllib.request, urllib.error, json, flask
 
-if is_it_raining_in_seattle == "true":
-    print("Yes!")
-elif is_it_raining_in_seattle == "false":
-    print("No!")
-else:
-    print("Unexpected response:", is_it_raining_in_seattle)
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def is_it_raining_in_seattle():
+    with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
+        is_it_raining_in_seattle = response.read().decode()
+
+    if is_it_raining_in_seattle == "true":
+        return "<p>Yes</p>"
+    else:
+        return "<p>No</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
